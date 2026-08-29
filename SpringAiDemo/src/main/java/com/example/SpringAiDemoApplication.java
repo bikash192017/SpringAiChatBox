30501f02c6f00062706a6c9bd44b4f13929508d2
bikash192017 <bikash776279@gmail.com>
2026-08-29 13:41:22 +0530

    Web Socket Fixing

--- SpringAiDemo/src/main/java/com/example/SpringAiDemoApplication.java
+++ SpringAiDemo/src/main/java/com/example/SpringAiDemoApplication.java
@@ -1,12 +1,7 @@
 package com.example;
 
-import com.example.entity.ChatConversation;
-import com.example.entity.ChatMessage;
-import com.example.repository.ChatConversationRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringAiDemoApplication {
@@ -16,43 +11,6 @@ public class SpringAiDemoApplication {
 	}
 
 
-    @Bean
-    CommandLineRunner testMongoDB(
-            ChatConversationRepository repository) {
 
-        return args -> {
-
-            System.out.println(
-                    "========== MONGODB TEST STARTED =========="
-            );
-
-            ChatConversation conversation =
-                    new ChatConversation(
-                            "test@gmail.com",
-                            "MongoDB Test"
-                    );
-
-            conversation.getMessages().add(
-                    new ChatMessage(
-                            "USER",
-                            "Testing MongoDB connection"
-                    )
-            );
-
-            ChatConversation saved =
-                    repository.save(conversation);
-
-            System.out.println(
-                    "MONGODB DOCUMENT SAVED"
-            );
-
-            System.out.println(
-                    "ID: " + saved.getId()
-            );
-
-            System.out.println(
-                    "========== MONGODB TEST COMPLETED =========="
-            );
-        };
     }
-    }
+
